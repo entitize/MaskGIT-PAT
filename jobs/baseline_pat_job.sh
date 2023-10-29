863068a48e3ab5edc2e249e9ae13ddab0fbfd52e
Madeline Shao <mshao@login1.cm.cluster>
2023-10-28 22:54:48 -0700

    Add support for grayscale

---
+++ jobs/baseline_pat_job.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Submit this script with: sbatch thefilename
+#SBATCH -A mlprojects
+
+#SBATCH --time=24:00:00   # walltime, timeout (if script runs longer than specified, it will timeout). Setting it higher results in lower priority on HPC
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --gres=gpu:1 # number of GPUs
+#SBATCH --mem-per-cpu=5G # memory per CPU core
+#SBATCH -J "grayscale_pat_job_torchvision"   # job name
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+
+# Needed to load miniconda
+source ~/.bashrc
+
+module load cuda/11.8
+
+cd ~/MaskGIT-PAT
+
+# NOTE(@kai): the training_vqgan script already uses cuda by default
+
+python training_vqgan.py \
+    --dataset-path /groups/mlprojects/pat/pat_signals/original_signals \
+    --batch-size 4 \
+    --experiment-name original_pat_grayscale_torchvision \
+    --save-img-rate 1000 \
+    --image-channels 1
