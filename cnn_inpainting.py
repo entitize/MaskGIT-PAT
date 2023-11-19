2d874cc3793bb238989246e0f6eab81baec9db7f
Nika Chuzhoy <nchuzhoy@gmail.com>
2023-11-19 03:50:43 -0800

    Merge branch 'master' of https://github.com/entitize/MaskGIT-PAT

--- cnn_inpainting.py
+++ cnn_inpainting.py
@@ -27,36 +27,6 @@ from sklearn.model_selection import train_test_split
 # get_ipython().run_line_magic('matplotlib', 'inline')
 # from mpl_toolkits.axes_grid1 import ImageGrid
 
-# # Prepare Dataset
-def load_and_crop(data_dir, file_name, original_width, original_height, target_size):
-    '''
-    This function was used to create the dataset in '/groups/mlprojects/pat/pat_cnn'.
-    It is not currently used anywhere below.
-    '''
-    print(f"Processing {file_name}")
-    # Load the image from the .npy file
-    image = np.load(os.path.join(data_dir, file_name))
-
-    # Randomly crop the image to the target size
-    left = np.random.randint(0, original_width - target_size[0] + 1)
-    top = np.random.randint(0, original_height - target_size[1] + 1)
-    right = left + target_size[0]
-    bottom = top + target_size[1]
-    # cropped_image = pil_image.crop((left, top, right, bottom))
-    cropped_image = image[top:bottom, left:right]
-
-    # Reshape to be expected size
-    cropped_image_array = np.array(cropped_image).reshape(*target_size, 1)
-    # normalize to be from 0 to 255 since that is what is expected below
-    normalized_image = (cropped_image_array - cropped_image_array.min()) * (255.0 / (cropped_image_array.max() - cropped_image_array.min()))
-
-    # Save the cropped image array to a new .npy file
-    new_file_name = f"cropped_{file_name}"
-    new_dir = '/groups/mlprojects/pat/pat_new'
-    np.save(os.path.join(new_dir, new_file_name), cropped_image_array)
-    # image needs to be unit8 and not float32 in order to do masking properly
-    return normalized_image.astype('uint8')
-
 
 
 # #### Data Generator with Patch Augmentation
@@ -112,27 +82,25 @@ class createAugment(keras.utils.Sequence):
       else:
         X_batch[i,] = masked_image/255
       y_batch[i] = self.y[idx]/255
-
     return X_batch, y_batch
 
   def __createMask(self, img):
     ## Prepare masking matrix
-    mask = np.full((self.dim[0],self.dim[1],self.n_channels), 255, np.uint8)
     if self.mask_type == "lv":
       # limited view masking - rectangle 1/3 the width of the image, centered in the middle
+      mask = np.full((self.dim[0],self.dim[1],self.n_channels), 255, np.uint8)
       w = self.dim[0] // 3
       x = self.dim[0] // 2
       cv2.line(mask,(x,0),(x,self.dim[1]),(1,1,1),w)
     elif self.mask_type == "sa":
-        # spatial aliasing masking - 1-pixel wide vertical line every n columns
-        thickness = 1
-        multiplier = np.random.randint(3, 6)
-        x_coords = np.linspace(0, self.dim[0] - 1, self.dim[0] // (thickness * multiplier), dtype=np.uint8)
-        for x in x_coords:
-        # Draw black line on the white mask
-          cv2.line(mask,(x,0),(x,self.dim[1]),(1,1,1),thickness)
+        # #   mask[:]
+        # spatial aliasing masking - 1-pixel wide unmasked vertical line every m columns
+        mask = np.full((64,64, 1), 0, np.uint8)
+        thickness = np.random.randint(2, 5)
+        mask[:, ::thickness, :]= 255
     else:
       # random "brush" strokes masking
+      mask = np.full((self.dim[0],self.dim[1],self.n_channels), 255, np.uint8)
       for _ in range(np.random.randint(1, 10)):
         # Get random x locations to start line
         x1, x2 = np.random.randint(1, self.dim[0]), np.random.randint(1, self.dim[0])
@@ -145,7 +113,6 @@ class createAugment(keras.utils.Sequence):
 
     # Perform bitwise and operation to mak the image
     masked_image = cv2.bitwise_and(img, mask)
-
     return masked_image
 
 ## For more information into formulation: https://www.youtube.com/watch?v=AZr64OxshLo
@@ -202,9 +169,9 @@ class PredictionLogger(tf.keras.callbacks.Callback):
     def __init__(self):
         super(PredictionLogger, self).__init__()
 
-    def on_epoch_end(self, logs, epoch):
-        sample_idx = 54
-        sample_images, sample_labels = testgen[sample_idx]
+    def save_wandb(self, generator, name):
+        sample_idx = 42
+        sample_images, sample_labels = generator[sample_idx]
 
         images = []
         labels = []
@@ -217,30 +184,23 @@ class PredictionLogger(tf.keras.callbacks.Callback):
             labels.append(sample_labels[i])
             predictions.append(inpainted_image.reshape(inpainted_image.shape[1:]))
 
-        wandb.log({"images": [wandb.Image(image)
+        wandb.log({f"{name}_images": [wandb.Image(image)
                               for image in images]})
-        wandb.log({"labels": [wandb.Image(label)
+        wandb.log({f"{name}_label": [wandb.Image(label)
                               for label in labels]})
-        wandb.log({"predictions": [wandb.Image(inpainted_image)
+        wandb.log({f"{name}_predictions": [wandb.Image(inpainted_image)
                               for inpainted_image in predictions]})
+
+    def on_epoch_end(self, logs, epoch):
+       self.save_wandb(valgen, 'val')
+       self.save_wandb(testgen, 'test')
 
 
 
 # # Testing on images
 
-# fig, axs = plt.subplots(nrows=rows, ncols=3, figsize=(6, 2*rows))
-
-# for i in range(32):
-#   impainted_image = model.predict(sample_images[i].reshape((1,)+sample_images[i].shape))
-#   axs[i][0].imshow(sample_labels[i])
-#   axs[i][1].imshow(sample_images[i])
-#   axs[i][2].imshow(impainted_image.reshape(impainted_image.shape[1:]))
-
-# plt.show()
-
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
-# import subprocess
 if __name__ == '__main__':
     print('[INFO]', tf.config.experimental.list_physical_devices('GPU'))
     parser = argparse.ArgumentParser(description="VQGAN")
@@ -260,19 +220,15 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     print(tf.__version__)
-    # set your wandb API key (https://docs.wandb.ai/quickstart)
-    # in ~/.bashrc, put
-    # export WANDB_API_KEY="your_api_key_here"
-    wandb.login(key=os.environ["WANDB_API_KEY"])
 
     if args.dataset_path != 'cifar':
-        # data_dir = "/groups/mlprojects/pat/pat_cnn"
-        # data = [load_and_crop(data_dir, f, original_width, original_height, target_size) for f in os.listdir(data_dir) if f.endswith('.npy')]
-        data = [np.load(os.path.join(args.dataset_path, f)) for f in os.listdir(args.dataset_path) if f.endswith('.npy')]
+        x_train = [(np.load(os.path.join(args.dataset_path, 'train', f)) * 255) for f in os.listdir(os.path.join(args.dataset_path, 'train')) if f.endswith('.npy')]
+        x_val = [(np.load(os.path.join(args.dataset_path, 'val', f)) * 255) for f in os.listdir(os.path.join(args.dataset_path, 'val')) if f.endswith('.npy')]
+        x_test = [(np.load(os.path.join(args.dataset_path, 'test', f)) * 255) for f in os.listdir(os.path.join(args.dataset_path, 'test')) if f.endswith('.npy')]
 
-        x_train, x_test = train_test_split(data, train_size=0.8, random_state=42)
-        x_train = np.array(x_train)
-        x_test = np.array(x_test)
+        x_train = np.array(x_train).astype('uint8')
+        x_val = np.array(x_val).astype('uint8')
+        x_test = np.array(x_test).astype('uint8')
     else:
        (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
        args.image_channels = 3
@@ -282,8 +238,10 @@ if __name__ == '__main__':
     Image.fromarray(np.squeeze(x_train[0]))
 
     print('x_train shape:', x_train.shape)
+    print('x_val shape:', x_val.shape)
     print('x_test shape:', x_test.shape)
     print(x_train.shape[0], 'train samples')
+    print(x_val.shape[0], 'val samples')
     print(x_test.shape[0], 'test samples')
 
     mask_type = "stroke"
@@ -294,23 +252,29 @@ if __name__ == '__main__':
 
     ## Prepare training and testing mask-image pair generator
     traingen = createAugment(x_train, x_train, dim=(args.image_size, args.image_size), n_channels=args.image_channels, mask_type=mask_type)
+    valgen = createAugment(x_val, x_val, shuffle=False, dim=(args.image_size, args.image_size), n_channels=args.image_channels, mask_type=mask_type)
     testgen = createAugment(x_test, x_test, shuffle=False, dim=(args.image_size, args.image_size), n_channels=args.image_channels, mask_type=mask_type)
 
     ## Save examples of what the masking looks like
-    sample_idx = 99 ## Change this to see different batches
-    sample_masks, sample_labels = traingen[sample_idx]
+    sample_idx = 42 ## Change this to see different batches
+    sample_masks, sample_labels = valgen[sample_idx]
     sample_images = [None]*(len(sample_masks)+len(sample_labels))
     sample_images[::2] = sample_labels
     sample_images[1::2] = sample_masks
     fig = plt.figure(figsize=(16., 8.))
     grid = ImageGrid(fig, 111,  # similar to subplot(111)
-                    nrows_ncols=(4, 8),  # creates 2x2 grid of axes
+                    nrows_ncols=(4, 8),  # creates 4x8 grid of axes
                     axes_pad=0.3,  # pad between axes in inch.
                     )
     for ax, image in zip(grid, sample_images):
-      ax.imshow(image)
+      ax.imshow(image, cmap='gray', vmin=0, vmax=1)
     os.makedirs("cnn", exist_ok=True)
     fig.savefig(f'cnn/cnn_masks_{args.experiment_name}.png')
+
+     # set your wandb API key (https://docs.wandb.ai/quickstart)
+    # in ~/.bashrc, put
+    # export WANDB_API_KEY="your_api_key_here"
+    wandb.login(key=os.environ["WANDB_API_KEY"])
 
     keras.backend.clear_session()
     model = inpaintingModel().prepare_model(input_size=(args.image_size, args.image_size, args.image_channels))
@@ -319,11 +283,12 @@ if __name__ == '__main__':
 
     # lmk if you want to be added to a shared project: https://wandb.ai/mshao0/pat-inpainting/overview
     wandb.init(project="pat-inpainting", name=args.experiment_name, config=args)
+
     _ = model.fit(traingen,
-            validation_data=testgen,
+            validation_data=valgen,
             epochs=args.epochs,
             steps_per_epoch=len(traingen),
-            validation_steps=len(testgen),
+            validation_steps=len(valgen),
             # use_multiprocessing=True,
             callbacks=[WandbCallback(),
                         PredictionLogger()])
